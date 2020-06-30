eebf4138c537a4208d75368bb750c33a38d07ad4
Ruslan Zaporojets <ruzzzua@gmail.com>
2020-06-30 09:33:47 +0300

    Fix test_benchmark

--- penchmark/tests/test_benchmark.py
+++ penchmark/tests/test_benchmark.py
@@ -1,4 +1,3 @@
-import time
 from io import StringIO
 from contextlib import redirect_stdout
 from copy import deepcopy
@@ -8,18 +7,14 @@ import pytest
 from penchmark import benchmark, Callee, InData
 
 
-def _long_op():
-    time.sleep(.00005)
-
-
 @pytest.mark.parametrize(
     'callees, dataset',
     [
         (
             # callees
             (
-                Callee('mul', lambda x: _long_op()),
-                Callee('nop', lambda x: None)
+                Callee('mul', lambda x: x[0] * x[1]),
+                Callee('nop', lambda x: x)
             ),
             # dataset
             (
@@ -68,6 +63,9 @@ def test_benchmark(callees, dataset):
     # float attrs
     for data_name in ['small-data', 'big-data']:
         for item_index in [0, 1]:
+            callee_name = report[data_name][item_index]['callee_name']
+            assert callee_name in ('nop', 'mul')
+
             for float_attr in ['elapsed', 'ratio']:
                 assert isinstance(report[data_name][item_index][float_attr], float)
                 assert report[data_name][item_index][float_attr] > 0
@@ -76,18 +74,21 @@ def test_benchmark(callees, dataset):
         assert report[data_name][0]['elapsed'] < report[data_name][1]['elapsed']
         assert report[data_name][0]['ratio'] < report[data_name][1]['ratio']
 
-    # clear float attrs
+    # clear attrs
     for data_name in ['small-data', 'big-data']:
         for item_index in [0, 1]:
+            report[data_name][item_index]['callee_name'] = 'op'
             for float_attr in ['elapsed', 'ratio']:
                 report[data_name][item_index][float_attr] = 0
+
+    # compare structure
     assert report == {
         'small-data': [
-            {'callee_name': 'nop', 'elapsed': 0, 'ratio': 0},
-            {'callee_name': 'mul', 'elapsed': 0, 'ratio': 0}
+            {'callee_name': 'op', 'elapsed': 0, 'ratio': 0},
+            {'callee_name': 'op', 'elapsed': 0, 'ratio': 0}
         ],
         'big-data': [
-            {'callee_name': 'nop', 'elapsed': 0, 'ratio': 0},
-            {'callee_name': 'mul', 'elapsed': 0, 'ratio': 0}
+            {'callee_name': 'op', 'elapsed': 0, 'ratio': 0},
+            {'callee_name': 'op', 'elapsed': 0, 'ratio': 0}
         ]
     }
